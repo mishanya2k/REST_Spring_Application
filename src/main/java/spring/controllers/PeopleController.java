89cc5ef267319fc5de4693b58bd128c9597afbda
mishanya2k <103316910+mishanya2k@users.noreply.github.com>
2023-05-15 19:51:07 +1000

    new methods(newPerson,create) new view

--- src/main/java/spring/controllers/PeopleController.java
+++ src/main/java/spring/controllers/PeopleController.java
@@ -3,11 +3,9 @@ package spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import spring.dao.PersonDAO;
+import spring.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +29,16 @@ public class PeopleController {
     public String showByIndex(@PathVariable("id") int id, Model model){
         model.addAttribute("person",personDAO.showByIndex(id));
         return "people/showByIndex";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
